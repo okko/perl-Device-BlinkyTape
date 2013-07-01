abdf76ed7c24fbc1336505ff18b7b95473952618
Oskari Okko Ojala <git@okko.fi>
2013-07-01 17:37:35 +0300

    Finalized implementation with the SimulationPort

--- lib/Device/BlinkyTape.pm
+++ lib/Device/BlinkyTape.pm
@@ -8,10 +8,9 @@ BEGIN {
 }
 use Moose;
 use Device::SerialPort;
+use Device::BlinkyTape::SimulationPort;
 use Moose::Util::TypeConstraints;
 use utf8;
-
-extends 'Plack::Middleware';
 
 subtype 'GammaInt',
     as 'Int',
@@ -19,38 +18,56 @@ subtype 'GammaInt',
     message { "Gamma number ($_) must be between 0..255" }
 ;
 
-has 'dev' => (is => 'rw', isa => 'String', default => '/dev/tty.usbmodem');
-has 'port' => (is => 'rw', isa => 'Device::SerialPort');
+subtype 'DeviceSerialPort',
+    as 'Device::SerialPort'
+;
+
+subtype 'SimulationPort',
+    as 'Device::BlinkyTape::SimulationPort'
+;
+
+has 'dev' => (is => 'rw', isa => 'Str', default => '/dev/tty.usbmodem');
+has 'port' => (is => 'rw', isa => 'DeviceSerialPort | SimulationPort');
 has 'gamma' => (is => 'rw', isa => 'ArrayRef[GammaInt]', default => sub { [1,1,1] });
 
 has 'led_count' => (is => 'rw', isa => 'Int', default => 60);
 
+has 'simulate' => (is => 'rw', isa => 'Bool', default => 0);
+
 sub BUILD {
     my $self = shift;
     # Initialize $self->port from $self->dev if one was not given in new
+    if ($self->simulate) {
+        $self->port(Device::BlinkyTape::SimulationPort->new(led_count => $self->led_count));
+        warn 'simulation on.';
+    }
     $self->port(Device::SerialPort->new($self->dev)) if (!$self->port);
     
-    # Set default communication settings
-    $self->port->baudrate(19200);
-    $self->port->databits(8);
-    $self->port->parity('none');
-    $self->port->stopbits(1);
+    if ($self->port) {
+        # Set default communication settings
+        $self->port->baudrate(19200);
+        $self->port->databits(8);
+        $self->port->parity('none');
+        $self->port->stopbits(1);
+    }
 
-    $self->lookclear; # empty buffers
+    # $self->lookclear; # empty buffers
 }
 
 sub all_on {
     my $self = shift;
-    for (my $a=0; $a<=$self->led_count; $a++) {
+    for (my $a=0; $a<=$self->led_count-1; $a++) {
         $self->send_pixel(255,255,255);
     }
+    $self->show();
 }
 
 sub all_off {
     my $self = shift;
-    for (my $a=0; $a<=$self->led_count; $a++) {
+    for (my $a=0; $a<=$self->led_count-1; $a++) {
         $self->send_pixel(0,0,0);
     }
+    $self->show();
 }
 
 1;
@@ -58,11 +75,16 @@ sub all_off {
 =head1 Usage
 
 use Device::BlinkyTape::WS2811; # BlinkyTape uses WS2811
-my $bb = Device::BlinkyTape->new(dev => '/dev/tty.usbmodem');
+my $bb = Device::BlinkyTape::WS2811->new(dev => '/dev/tty.usbmodem');
 $bb->all_on();
 $bb->all_off();
 $bb->send_pixel(255,255,255);
-$b->send_pixel(5,5,5);
+$bb->send_pixel(5,5,5);
+$bb->show(); # shows the sent pixel row
+
+=head1 Usage on OS X without a BlinkyTape
+
+Install X11 server from http://xquartz.macosforge.org/landing/
 
 =head1 Reference reading
  * Communicating with the Arduino in Perl http://playground.arduino.cc/interfacing/PERL
