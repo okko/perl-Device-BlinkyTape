8a80d14be0e467e6086fec18a25ce51bb22e0f2d
Oskari Okko Ojala <git@okko.fi>
2013-12-12 00:43:02 +0200

    updated experiment code, now without sleeping

--- example/experiment.pl
+++ example/experiment.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/env perl
 use lib '../lib';
 use Device::BlinkyTape::WS2811; # BlinkyTape uses WS2811
-use Time::HiRes (usleep);
 
 my $bb = Device::BlinkyTape::WS2811->new(dev => '/dev/tty.usbmodem1411');
 
@@ -9,13 +8,6 @@ for (my $b=0; $b<=100000; $b++) {
     for (my $a=0; $a<=59; $a++) {
         $bb->send_pixel(int(rand(254)),int(rand(254)),int(rand(254)));
     }
-    $bb->show(); # shows the sent pixel row. One should be enough, six works longer? :)
-    $bb->show(); # shows the sent pixel row
-    $bb->show(); # shows the sent pixel row
-    $bb->show(); # shows the sent pixel row
-    $bb->show(); # shows the sent pixel row
-    $bb->show(); # shows the sent pixel row
-    usleep(100000);
-    print "$b\n";
+    $bb->show(); # shows the sent pixel row.
 }
-sleep 2;
+
