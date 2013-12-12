a794ec32fa5612a1c8bf69b32040f114e7c14e1e
Oskari Okko Ojala <git@okko.fi>
2013-12-12 22:31:13 +0200

    renamed experiment to all-lights-random, for better description

--- example/all-lights-random.pl
+++ example/all-lights-random.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use lib '../lib';
 use Device::BlinkyTape::WS2811; # BlinkyTape uses WS2811
-
+use Time::HiRes qw / usleep /;
 my $bb = Device::BlinkyTape::WS2811->new(dev => '/dev/tty.usbmodem1411');
 
 for (my $b=0; $b<=100000; $b++) {
@@ -9,5 +9,6 @@ for (my $b=0; $b<=100000; $b++) {
         $bb->send_pixel(int(rand(254)),int(rand(254)),int(rand(254)));
     }
     $bb->show(); # shows the sent pixel row.
+    usleep(400000);
 }
 
