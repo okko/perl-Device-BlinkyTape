7f6c7ba7f91320c07c557316aafa21e3477b1c2d
Oskari Okko Ojala <git@okko.fi>
2013-12-25 01:21:36 +0200

    Support multiple BlinkyTapes as arguments.

--- example/christmas-blinking.pl
+++ example/christmas-blinking.pl
@@ -4,12 +4,12 @@ use lib '../lib';
 use Device::BlinkyTape::WS2811; # BlinkyTape uses WS2811
 use Time::HiRes qw/ usleep /;
 
-my $dev = shift @ARGV;
+my $devpeek = $ARGV[0];
 
-if (!$dev) {
+if (!$devpeek) {
     print "\n";
-    print "Usage: $0 [device] [amount_of_blinking] [blinking_speed_min] [blinking_speed_max]\n";
-    print "   [device] is the file to BlinkyTape, for example /dev/tty.usbmodem1411\n";
+    print "Usage: $0 [device ..] [amount_of_blinking] [blinking_speed_min] [blinking_speed_max]\n";
+    print "   [device] is the file or files to BlinkyTape, for example /dev/tty.usbmodem1411\n";
     print "\n";
     print "   Default is            $0 [device] 30  1 30\n";
     print "   For faster blinking:  $0 [device] 30 30 30\n";
@@ -18,14 +18,29 @@ if (!$dev) {
     exit 255;
 }
 
+my @dev;
+while ($ARGV[0] =~ /^\/dev/) {
+    my $sdev = shift @ARGV;
+    push @dev, $sdev;
+}
+use Data::Dumper;
+print 'Devices: '.Dumper(\@dev);
+
 my $amount_of_blinking = shift @ARGV || 30;
 my $brightness_step_min = shift @ARGV || 1;
 my $brightness_step_max = shift @ARGV || 30;
 
-my $bb = Device::BlinkyTape::WS2811->new(dev => $dev);
+my @bb;
+foreach my $sdev (@dev) {
+    my $sbb = Device::BlinkyTape::WS2811->new(dev => $sdev);
+    push @bb, $sbb;
+}
 
-my @led; # 0..59
-for (my $a=0; $a<=59; $a++) {
+my $device_count = scalar(@dev);
+my $maxled = (60 * $device_count - 1);
+
+my @led;
+for (my $a=0; $a<=$maxled; $a++) {
     $led[$a] = 0;
 }
 my @ledb;
@@ -33,7 +48,7 @@ my @ledr;
 
 for (my $b=0; $b<=100000; $b++) {
     if ($b % $amount_of_blinking == 0) {
-        my $pos = int(rand(59));
+        my $pos = int(rand($maxled)+1);
         if ($led[$pos]==0) { # if not already blinking
             my $brightness_step = int(rand($brightness_step_max-$brightness_step_min))+$brightness_step_min;
             $ledb[$pos] = $brightness_step;
@@ -42,7 +57,7 @@ for (my $b=0; $b<=100000; $b++) {
         }
     }
 
-    for (my $a=0; $a<=59; $a++) {
+    for (my $a=0; $a<=$maxled; $a++) {
         if ($led[$a]>0) {
             $led[$a] = $led[$a]+$ledb[$a];
         }
@@ -56,10 +71,12 @@ for (my $b=0; $b<=100000; $b++) {
 
         my $o = $sendval-$ledr[$a]; # redness
         $o=0 if ($o<0);
-
-        $bb->send_pixel($sendval, $o, $o);
+        
+        $bb[$a % $device_count]->send_pixel($sendval, $o, $o);
     }
-    $bb->show(); # shows the sent pixel row.
+    foreach my $sbb (@bb) {
+        $sbb->show(); # shows the sent pixel row.
+    }
     usleep(400);
 }
 
